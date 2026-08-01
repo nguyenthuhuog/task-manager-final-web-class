8e2f8efd8157e3f90348fa04237ddd27be419380
nguyenthuhuog <Huong.NT210423@gmail.com>
2026-08-02 00:31:49 +0700

    add api base

--- front_end/js/api.js
+++ front_end/js/api.js
@@ -2,7 +2,7 @@
 // API Configuration
 // =====================================
 
-const API_BASE = "http://localhost:3000/api";
+const API_BASE = "https://task-manager-final-web-class-back-end.onrender.com/api";
 
 
 // =====================================
